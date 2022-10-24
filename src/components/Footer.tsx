de97c099346c361ba40cbb25225671a8ddcde4cd
Wesllen Vinicius <wesllenvinicius09@gmail.com>
2022-10-24 16:12:34 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -69,18 +69,20 @@ export function Footer() {
           ">
               <img src="src/resources/images/logo-filled.svg" className="h-14" />
             </h6>
-            <p className="font-bold">
+            <p className="font-bold ">
               COTRIPAM IND. E COM. DE TRIPAS
             </p>
+
             <p className="font-bold">
               PORTAL DO AMAZONAS LTDA
             </p>
           </div>
           <div className="">
-            <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
+            <h6 className="uppercase font-semibold mb-0 flex justify-center md:justify-start">
               produtos
             </h6>
-            <p className="mb-4">
+            <div className="border-b border-gray-300 w-44"></div>
+            <p className="mb-4 mt-4">
               <a href="#!" className="text-white">Salgados</a>
             </p>
             <p className="mb-4">
@@ -91,10 +93,11 @@ export function Footer() {
             </p>
           </div>
           <div className="">
-            <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
+            <h6 className="uppercase font-semibold flex justify-center md:justify-start">
               links uteis
             </h6>
-            <p className="mb-4">
+            <div className="border-b border-gray-300 w-44"></div>
+            <p className="mb-4 mt-4">
               <a href="#!" className="text-white">Pricing</a>
             </p>
             <p className="mb-4">
@@ -108,12 +111,13 @@ export function Footer() {
             </p>
           </div>
           <div className="">
-            <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
+            <h6 className="uppercase font-semibold  flex justify-center md:justify-start">
               contatos
             </h6>
-            <p className="flex items-center justify-center md:justify-start mb-4">
+            <div className="border-b border-gray-300 w-44"></div>
+            <p className="flex items-center  justify-center md:justify-start mb-4 mt-4">
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home"
-                className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                className=" text-white w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                 <path fill="currentColor"
                   d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
                 </path>
